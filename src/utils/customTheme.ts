c133c41f2d55ec5501e19befacec2fb3e43296f4
Rafael Candido <rafacandido0708@gmail.com>
2024-01-04 16:34:33 -0300

    apx

---
+++ src/utils/customTheme.ts
@@ -0,0 +1,42 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const customTheme = extendTheme({
+  colors: {
+    primary: {
+      50: '#e0fbfd',
+      100: '#bfeced',
+      200: '#9ddddf',
+      300: '#79cfd3',
+      400: '#55c2c6',
+      500: '#3da8ad',
+      600: '#2c8386',
+      700: '#1c5d60',
+      800: '#08393b',
+      900: '#001515',
+    },
+    secondary: {
+      50: '#e0f8fd',
+      100: '#bfe9ed',
+      200: '#9ddbdf',
+      300: '#79cfd3',
+      400: '#55bac6',
+      500: '#3d99ad',
+      600: '#2c7186',
+      700: '#1c4b60',
+      800: '#08283b',
+      900: '#000d15',
+    },
+    optional: {
+      50: '#e0fdfc',
+      100: '#bfedec',
+      200: '#9ddfdf',
+      300: '#79cfd3',
+      400: '#55c6c2',
+      500: '#3dada3',
+      600: '#2c8677',
+      700: '#1c6051',
+      800: '#083b2d',
+      900: '#00150d',
+    },
+  },
+})
